127eb31cee4d88898dd21e16f9c5dde1efb2a028
swrko <inviiik@gmail.com>
2020-03-26 13:55:31 +0100

    init commit

---
+++ hough.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+# acumulator = np.array([np.arange(1, 20),np.arange(1, 20)], np.uint64)
+acumulator = np.zeros([20, 20], dtype=int)  # acumulator ako pole 20x20 typu int
+fi = np.deg2rad(np.arange(0, 180, 9)) # fi ako kroky uhlov
+
+
+
